e694a9f86563d75ede44a2ab5ba3d0b5e94c3dd2
Yixuan Qiu <yixuan.qiu@cos.name>
2015-12-04 15:48:42 -0500

    a simple implementation

---
+++ src/main/scala/SimpleCG.scala
@@ -0,0 +1,15 @@
+package statr.stat695ss
+
+import breeze.linalg._
+import breeze.numerics._
+import scala.util.control._
+
+
+class SimpleCG(val mat_A: DenseMatrix[Double], vec_b: DenseVector[Double])
+    extends ConjugateGradient(mat_A.rows, vec_b) {
+
+    // Matrix-vector product to be implemented
+    protected def mat_prod(x: DenseVector[Double]): DenseVector[Double] = {
+        return mat_A * x
+    }
+}
