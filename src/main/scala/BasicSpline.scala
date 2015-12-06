a6fbdca422673849550c3b5a911ef5678b685299
Yixuan Qiu <yixuan.qiu@cos.name>
2015-12-06 17:05:16 -0500

    lambda tuning

--- src/main/scala/BasicSpline.scala
+++ src/main/scala/BasicSpline.scala
@@ -3,6 +3,7 @@ package statr.stat695ss
 import breeze.linalg._
 import breeze.numerics._
 import scala.util.control._
+import scala.util.Random
 
 object BasicSpline {
     // Bernoulli polynomials
@@ -26,6 +27,15 @@ object BasicSpline {
             res(i) = Rkernel(x(i), y(i))
         }
         return res.dot(weights)
+    }
+
+    // Generate lambda sequence
+    def generate_lambda(lmax: Double = 1.0, lmin: Double = 1e-5, nlambda: Int = 10): Array[Double] = {
+        val logmin = math.log(lmin)
+        val logmax = math.log(lmax)
+        val step = (logmax - logmin) / (nlambda - 1.0)
+        val lambda = (0 until nlambda).map(i => math.exp(logmax - i * step)).toArray
+        return lambda
     }
 }
 
@@ -105,6 +115,42 @@ class BasicSpline(val dat_x: DenseMatrix[Double],
         this.maxit = maxit
         this.eps = eps
         solver.set_opts(maxit, eps)
+    }
+
+    private def Vscore(lambda: Double, w: DenseVector[Double], Ttw: DenseVector[Double],
+                       init: DenseVector[Double]): (Double, DenseVector[Double]) = {
+        this.lambda = lambda
+        solver.solve(Ttw)
+        val w_hat = this.pred
+
+        solver.solve(Tty, init)
+        val resid = dat_y - this.pred
+
+        val denom = 1.0 - w.dot(w_hat) / dim_n.toDouble
+        val vscore = resid.dot(resid) / dim_n.toDouble / denom / denom
+        return (vscore, solver.coef)
+    }
+
+    def tune(lmax: Double, lmin: Double, nlambda: Int = 10, seed: Int = 1): (DenseVector[Double], DenseVector[Double]) = {
+        val lambdas = BasicSpline.generate_lambda(lmax, lmin, nlambda)
+        val vscore = DenseVector.zeros[Double](nlambda)
+
+        val r = new Random(seed)
+        val w = new DenseVector((0 until dim_n).map(x => r.nextGaussian()).toArray)
+        val Ttw = DenseVector.zeros[Double](dim_m + dim_n)
+        Ttw(0 until dim_m) := S.t * w
+        Ttw(dim_m to -1) := Q * w
+
+        // Cache the result from previous lambda
+        val last_sol = DenseVector.zeros[Double](dim_m + dim_n)
+
+        for (i <- 0 until nlambda) {
+            val res = Vscore(lambdas(i), w, Ttw, last_sol)
+            vscore(i) = res._1
+            last_sol := res._2
+        }
+
+        return (new DenseVector(lambdas), vscore)
     }
 
     def fit(lambda: Double) {
