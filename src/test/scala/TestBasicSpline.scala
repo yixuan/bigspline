11e15b8decb7494849a411f27a0b98bb5d772a42
Yixuan Qiu <yixuan.qiu@cos.name>
2015-12-06 15:23:10 -0500

    use example data

--- src/test/scala/TestBasicSpline.scala
+++ src/test/scala/TestBasicSpline.scala
@@ -6,14 +6,16 @@ import breeze.numerics._
 
 class TestBasicSpline extends TestBase {
 
-    val n = 20
-    val p = 3
-    val x = DenseMatrix.rand(n, p)
-    val y = DenseVector.rand(n)
+    val f = "other/dat.txt"
+    val (x, y) = read_data(f)
 
     test("Basic Spline") {
         val mod = new BasicSpline(x, y, 0.001)
         mod.fit()
+        info("Coefficients = ")
         info(format_vec(mod.coef))
+        info("Predicted values = ")
+        info(format_vec(mod.pred))
+        info("# of iterations = " + mod.niter)
     }
 }
